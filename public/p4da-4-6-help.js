7e2ba1597937672a14bd80588d2f11eaeb94c1c2
DanielHGroner <dhgroner@yahoo.com>
2024-06-11 11:21:24 -0400

    Add files via upload
    remaining example ch4 (4-6)

---
+++ public/p4da-4-6-help.js
@@ -0,0 +1,37 @@
+const allhelp = { 
+     "title":"This program calculates BMI based on a height and different weights. "+
+        "After prompting for height and weight, the while statement is used to "+
+        "provide repetition, to prompt for more weights.  BMI is calculated and displayed "+
+        "for each weight.  The repetition ends after the user enters a 0 for the weight.",
+     "5": "Get input for height, convert to float, "+
+         "and assign to variable <span class='cx-var'>height</span>. "+ 
+         "The <span class='cx-fun'>input()</span> function returns a string, which is converted to a number "+
+         "by the <span class='cx-fun'>float()</span> function, before being assigned to the <span class='cx-var'>height</span> variable.",
+     "8": "Get an initial input for weight, convert to float, "+
+         "and assign to variable <span class='cx-var'>weight</span>. "+
+         "The <span class='cx-fun'>input()</span> function returns a string, which is converted to a number " +
+         "by the <span class='cx-fun'>float()</span> function, before being assigned to the <span class='cx-var'>weight</span> variable.",
+     "11": "The <span class='cx-key'>while</span> statement checks if the variable <span class='cx-var'>weight</span> is not 0; "+
+         " if so, the indented statements will execute, after which flow returns back to the <span class='cx-key'>while</span> statement.",
+     "12": "Calculate <span class='cx-var'>bmi</span> based on "+
+         "<span class='cx-var'>weight</span> and "+
+         "<span class='cx-var'>height</span>.",
+     "13": "Display the calculated BMI.  The <span class='cx-fun'>print()</span> function here displays "+
+         "both a string, as well as the value stored in the variable "+
+         "<span class='cx-var'>bmi</span>, rounded to 1 decimal place.",
+     "15": "Get subsequent input for weight, convert to float, "+
+         "and assign to variable <span class='cx-var'>weight</span>. "+
+         "The <span class='cx-fun'>input()</span> function returns a string, which is converted to a number " +
+         "by the <span class='cx-fun'>float()</span> function, before being assigned to the <span class='cx-var'>weight</span> variable.",
+ 
+    "height": "This variable stores height (in inches). "+
+       "It is entered by the user, and subsequently used in the calculation of BMI ",
+    "weight": "This variable stores weight (in pounds). "+
+       "It is entered by the user, and subsequently used in the calculation of BMI ",
+    "bmi": "This variable stores bmi. "+
+       "It is calculated based on height and weight, and then displayed.",
+
+       "display": "-", 
+       "keyboard": "-"   
+};
+  
---
+++ public/p4da-4-6-hilite.js
@@ -0,0 +1,19 @@
+const allhilite2 = {
+   "5": [">height", ">display", "<keyboard"],
+   "8": [">weight", ">display", "<keyboard"],
+   "11": [">weight"],
+   "12": ["<weight", "<height", ">bmi"],
+   "13": ["<bmi", ">display"],
+   "15": [">weight", ">display", "<keyboard"],
+
+   "height": ["5", "12"],
+   "weight": ["8", "11", "12", "15"],
+   "bmi": ["12", "13"],
+
+   "display": ["5", "8", "13", "15"],
+   "keyboard": ["5", "8", "15"]
+};
+
+const allhilite3 = {
+   "11": ["12", "13", "15"]
+};
